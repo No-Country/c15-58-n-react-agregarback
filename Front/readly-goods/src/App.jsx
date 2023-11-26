74df5d63a54b4dc3a25dde6d8a624983217c9fd2
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2023-11-26 17:35:45 -0300

    instale tailwind y cree componente navbar

--- Front/readly-goods/src/App.jsx
+++ Front/readly-goods/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
-import './App.css'
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1 >Readly Goods</h1>
-      </div>
-      
-    </>
-  )
+    <div className="flex flex-col ">
+      <Navbar/>
+      <main className=" h-96 flex items-center justify-center">
+        <h3>Items Container</h3>
+      </main>
+      <footer className="bg-green-300">
+        <h4>Footer</h4>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
