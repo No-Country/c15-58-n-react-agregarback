8bcb96a8a3d4f0378858c6b90d7e33aff2bb379e
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2023-11-27 19:31:08 -0300

    agregue un logo provisorio

--- Front/readly-goods/src/components/Navbar.jsx
+++ Front/readly-goods/src/components/Navbar.jsx
@@ -4,14 +4,21 @@ import { faCartShopping , faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-black text-white h-20 items-center justify-between">
-        <h1 className=" text-3xl font-bold uppercase p-5">Readly Goods</h1>
+    <nav className="flex bg-black text-white h-24 items-center justify-between">
+        
+        <a className='p-5' href="/">
+            <img 
+                className='h-20 w-44'
+                src="src/assets/Logo2.png" 
+                alt="logo" 
+            />
+        </a>
         <ul className="flex items-center justify-end">
           <li className="uppercase p-3">inicio</li>
           <li className="uppercase p-4">catalogo</li>
           <li className="uppercase p-4">compras online</li>
-          <li className="u p-5"><FontAwesomeIcon icon={faUser} /></li>    {/*-----login----*/}
-          <li className=" p-9"><FontAwesomeIcon icon={faCartShopping} /></li>  {/*-----CARRITO DE COMPRA----*/}
+          <li className="p-5"><FontAwesomeIcon className="h-5" icon={faUser} /></li>    {/*-----login----*/}
+          <li className=" p-9"><FontAwesomeIcon className="h-5" icon={faCartShopping} /></li>  {/*-----CARRITO DE COMPRA----*/}
         </ul>
       </nav>
   )
