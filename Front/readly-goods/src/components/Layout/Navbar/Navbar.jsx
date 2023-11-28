8b7dd23ab53b17a52f80a97e3fbb476a9f4741b1
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2023-11-28 20:31:55 -0300

    instale router-dom creamos componentes Nav layout y pages

--- Front/readly-goods/src/components/Layout/Navbar/Navbar.jsx
+++ Front/readly-goods/src/components/Layout/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping , faUser} from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -14,11 +15,11 @@ const Navbar = () => {
             />
         </a>
         <ul className="flex items-center justify-end">
-          <li className="uppercase p-3">inicio</li>
-          <li className="uppercase p-4">catalogo</li>
-          <li className="uppercase p-4">compras online</li>
-          <li className="p-5"><FontAwesomeIcon className="h-5" icon={faUser} /></li>    {/*-----login----*/}
-          <li className=" p-9"><FontAwesomeIcon className="h-5" icon={faCartShopping} /></li>  {/*-----CARRITO DE COMPRA----*/}
+          <li className="uppercase p-3"><Link to='/'>inicio</Link></li>
+          <li className="uppercase p-4"><Link to='/books'>catalogo</Link></li>
+          <li className="uppercase p-4"><Link to='/faq'>compras online</Link></li>
+          <li className="p-5"><Link to='/user'><FontAwesomeIcon className="h-5" icon={faUser} /></Link></li>    {/*-----login----*/}
+          <li className=" p-9"><Link to='/cart'><FontAwesomeIcon className="h-5" icon={faCartShopping} /></Link></li>  {/*-----CARRITO DE COMPRA----*/}
         </ul>
       </nav>
   )
