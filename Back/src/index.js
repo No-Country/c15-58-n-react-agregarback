24119cfb434d1122a9852c2c30b71a08cd7456c4
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2023-11-29 00:09:55 -0300

    Update index.js

--- Back/src/index.js
+++ Back/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import { connectDB } from './db/config.js'
 import { userRouter } from './routers/users.router.js'
+import { booksRouter } from './routers/books.router.js'
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // endpoints
 app.use('/api/v1/user', userRouter)
+app.use('/api/v1/books', booksRouter)
 
 connectDB()
 
