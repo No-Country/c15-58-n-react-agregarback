8671630629a4a1b173ad4c98632a8c5eae53e41d
michqiueli <michiqueli@gmail.com>
2023-11-29 10:15:12 -0300

    Cambio de controllers de books a bloques Try-Catch

--- Back/src/controllers/books.controllers.js
+++ Back/src/controllers/books.controllers.js
@@ -1,66 +1,61 @@
-import { Book} from "../models/book.model.js";
+import { Book } from "../models/book.model.js";
 
-export const postBook = (req, res) => {
-  const data = req.body;
-  const book = new Book(data);
-  book
-    .save()
-    .then((savedBook) => {
-      res.status(200).json(savedBook);
-    })
-    .catch((err) => {
-      res.status(400).send("Verifique que todos los datos esten completos o que el libro no exista ya en la Base de Datos");
-    });
+export const getAllBooks = async (req, res) => {
+  try {
+    const result = await Book.find({});
+
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
-export const getAllBooks = (req, res) => {
-  Book.find({})
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+export const postBook = async (req, res) => {
+  try {
+    const data = req.body;
+    const newBook = new Book(data);
+
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
-export const updateBook = (req, res) => {
-  const id = req.params.id;
-  console.log(id)
-  const data = req.body;
-  console.log(data)
-  Book.updateOne(
-    { _id: id },
-    {
-      $set: data,
-    }
-  )
-  .then((result) => {
-    res.status(200).send("EL libro ha sido modificado con exito");
-  })
-  .catch((err) => {
-    res.status(400).send(err);
-  });
-};
-
-export const getBookById = (req, res) => {
-  const id = req.params.id;
-  Book.find({ _id: id })
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-};
-
-export const deleteBook = (req, res) => {
+export const updateBook = async (req, res) => {
+  try {
     const id = req.params.id;
-    console.log(id)
-    Book.deleteOne({_id: id})
-    .then((result) => {
-        res.status(200).send("El libro fue eliminado con exito");
-      })
-      .catch((err) => {
-        res.status(400).send(`El libro con el ID: ${id} no existe`);
-      });
-}
+    const data = req.body;
+
+    await Book.updateOne(
+      { _id: id },
+      {
+        $set: data,
+      }
+    );
+    res.status(201).send("EL libro ha sido modificado con exito");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+export const getBookById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Book.find({ _id: id });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).send(`No existe un libro con ese ID`);
+  }
+};
+
+export const deleteBook = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Book.deleteOne({ _id: id });
+    res.status(200).send("El libro fue eliminado con exito");
+  } catch (error) {
+    res.status(400).send(`No existe un libro con ese ID`);
+  }
+};
