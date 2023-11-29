4e90884d42d4e8e69247536711f99a3e5907b3c8
michqiueli <michiqueli@gmail.com>
2023-11-28 21:17:22 -0300

    CRUD de Libros completo

--- Back/src/controllers/bookscontrollers.js
+++ Back/src/controllers/bookscontrollers.js
@@ -1,29 +1,74 @@
-const mongoose = require("mongoose")
-const Book = require ("../models/Book")
+const Book = require("../models/Book");
 
-const postBook = (req, res ) => {
-    const data = req.body
-    const book = new Book(data)
-    book.save()
-    .then(savedBook => {
-        res.status(200).json(savedBook)
+const postBook = (req, res) => {
+  const data = req.body;
+  const book = new Book(data);
+  book
+    .save()
+    .then((savedBook) => {
+      res.status(200).json(savedBook);
     })
-    .catch(err => {
-        res.status(400).send(err)
-    })
-}
+    .catch((err) => {
+      res.status(400).send("Verifique que todos los datos esten completos o que el libro no exista ya en la Base de Datos");
+    });
+};
 
-const getAllBooks = (req, res) =>{
-Book.find({})
-.then(result=>{
-     res.status(200).json(result)
- })
- .catch(err=>{
-     res.status(400).send(err)
- })
+const getAllBooks = (req, res) => {
+  Book.find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const updateBook = (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const data = req.body;
+  console.log(data)
+  Book.updateOne(
+    { _id: id },
+    {
+      $set: data,
+    }
+  )
+  .then((result) => {
+    res.status(200).send("EL libro ha sido modificado con exito");
+  })
+  .catch((err) => {
+    res.status(400).send(err);
+  });
+};
+
+const getBookById = (req, res) => {
+  const id = req.params.id;
+  Book.find({ _id: id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const deleteBook = (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    Book.deleteOne({_id: id})
+    .then((result) => {
+        res.status(200).send("El libro fue eliminado con exito");
+      })
+      .catch((err) => {
+        res.status(400).send(`El libro con el ID: ${id} no existe`);
+      });
 }
 
 module.exports = {
-    postBook, 
-    getAllBooks
-}
+  postBook,
+  getAllBooks,
+  updateBook,
+  getBookById,
+  deleteBook
+};
