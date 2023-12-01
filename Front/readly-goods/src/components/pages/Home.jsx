88ed06259296b094d1ca93e39322895c2bd0f4a7
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2023-11-30 22:01:50 -0300

    Update Home.jsx

--- Front/readly-goods/src/components/pages/Home.jsx
+++ Front/readly-goods/src/components/pages/Home.jsx
@@ -8,10 +8,8 @@ const Home = () => {
       <div className="h-[500px] text-4xl text-center font-bold">
         <Carrousel />
       </div>
-      <div className="h-96 text-4xl text-center font-bold">
       <Presentation />
     </div>
-      </div>
   );
 };
 export default Home;
