96fe75336071bd1721c8f25be9429a79c6df34b7
unknown <elmati.08@hotmail.com>
2023-12-05 16:51:15 -0300

    termine la validacion del signin

--- Front/readly-goods/src/context/index.jsx
+++ Front/readly-goods/src/context/index.jsx
@@ -11,23 +11,6 @@ const initialForm = {
   email: "",
   password: "",
 };
-const validationsForm = (form) => {
-  let errors = {};
-  let regExpUsuario = /^[a-zA-Z0-9_\-]+$/;
-  var regExpFullName = /^[a-zA-Z\u00C0-\u00FF\s]+$/;
-  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-
-  if(!regExpUsuario.test(form.userName.trim())){
-    errors.userName = 'No usar espacios ni caracteres especiales solo se acepta letras, numeros y guiones.'
-  }
-  if(!regExpFullName.test(form.fullName.trim())){
-    errors.fullName = 'No usar numeros ni caracteres especiales solo se acepta letras y espacios.'
-  }
-  if(!regexEmail.test(form.email.trim())){
-    errors.email = 'No usarolo se acepta letras y espacios.'
-  }
-  return errors;
-};
 //----------------------------------------------------------------------------------------------
 
 
@@ -49,9 +32,14 @@ const ContextProvider = ({ children }) => {
     loading,
     response,
     handleChange,
-    handleKeyUp,
+    handleKeyUpUser,
+    handleKeyUpFullName,
+    handleOnFocusEmail,
+    handleOnBlurEmail,
+    handleOnBlurPassword,
+    handleOnFocusPassword,
     handleSubmit,
-  } = useForm(initialForm, validationsForm);
+  } = useForm(initialForm);
 
   //----------------------------------------------------------------------------------------------
   
@@ -69,7 +57,12 @@ const ContextProvider = ({ children }) => {
         loading,
         response,
         handleChange,
-        handleKeyUp,
+        handleKeyUpUser,
+        handleKeyUpFullName,
+        handleOnFocusEmail,
+        handleOnBlurEmail,
+        handleOnBlurPassword,
+        handleOnFocusPassword,
         handleSubmit,
       }}
     >
