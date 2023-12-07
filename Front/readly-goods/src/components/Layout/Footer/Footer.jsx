01f6de4bf2a9029c97210879a3c4654494411059
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-12-07 20:44:56 -0300

    books carrousel

--- Front/readly-goods/src/components/Layout/Footer/Footer.jsx
+++ Front/readly-goods/src/components/Layout/Footer/Footer.jsx
@@ -6,22 +6,22 @@ import logoPya from "../../../assets/pedidosya.png";
 const Footer = () => {
   return (
     <footer className="bg-[#822626] w-full">
-      <div className="w-3/5 m-auto flex justify-between p-4 items-center">
-        <div className="flex justify-center items-center">
+      <div className="flex items-center justify-between w-3/5 p-4 m-auto">
+        <div className="flex items-center justify-center">
           <p className="m-2 text-white"></p>
-          <p className="h-10 w-10 m-1">
+          <p className="w-10 h-10 m-1">
             <img src={logoPya} alt="Pedidos Ya" />
           </p>
-          <p className="h-10 w-10 m-1">
+          <p className="w-10 h-10 m-1">
             <img src={logoDac} alt="DAC" />
           </p>
         </div>
-        <p className="m-2 text-white flex items-center">
+        <p className="flex items-center m-2 text-white">
           <span className="font-roboto-serif lg:text-2xl">Readly</span>
-          <img className="h-auto w-24" src={logo} alt="Logo" />
+          <img className="w-24 h-auto" src={logo} alt="Logo" />
           <span className="font-roboto-serif lg:text-2xl">Goods</span>
         </p>
-        <p className=" text-white m-2 text-lg">
+        <p className="m-2 text-lg text-white ">
           © Copyright 2023 / ReadlyGoods
         </p>
       </div>
