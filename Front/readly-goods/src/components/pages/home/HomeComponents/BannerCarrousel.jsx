9fcb7a982a0140223cbfc5250b22a4d9be65b4ee
michqiueli <michiqueli@gmail.com>
2023-12-21 00:23:01 -0300

    Link a los libros del banner

--- Front/readly-goods/src/components/pages/home/HomeComponents/BannerCarrousel.jsx
+++ Front/readly-goods/src/components/pages/home/HomeComponents/BannerCarrousel.jsx
@@ -3,15 +3,24 @@ import image2 from "../../../../assets/banner-lourdes-ferro-g.webp";
 import image3 from "../../../../assets/banner-manini-el-comandante-sin.webp";
 import image4 from "../../../../assets/banner-red-grande.webp";
 import { Carousel } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const BannerCarrousel = () => {
   return (
     <div className="h-36 sm:h-64 md:h-80 lg:h-96 rounded-none">
       <Carousel id="carousel">
-        <img className="h-full" src={image1} alt="Banner Image 1" />
-        <img className="h-full" src={image2} alt="Banner Image 2" />
-        <img className="h-full" src={image3} alt="Banner Image 3" />
-        <img className="h-full" src={image4} alt="Banner Image 4" />
+        <Link to="/books/6583a4a9673a15210d1129d0"className="h-full">
+          <img  className=" h-full w-full" src={image1} alt="Banner Image 1" />
+        </Link>
+        <Link to="/books/6583a4fe673a15210d1129db" className="h-full">
+          <img  className=" h-full w-full" src={image2} alt="Banner Image 1" />
+        </Link>
+        <Link to="/books/657f446aff5ec868b28f6e53"className="h-full">
+          <img  className=" h-full w-full" src={image3} alt="Banner Image 1" />
+        </Link>
+        <Link to="/books/6583a5412106cc44fa343a96" className="h-full">
+          <img  className=" h-full w-full" src={image4} alt="Banner Image 1" />
+        </Link>
       </Carousel>
     </div>
   );
