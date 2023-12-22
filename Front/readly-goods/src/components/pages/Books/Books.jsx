5f45dd0cb5cefb759fb98fc8aeca5f82b8e4b48d
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2023-12-21 21:24:20 -0500

    Update Books.jsx

--- Front/readly-goods/src/components/pages/Books/Books.jsx
+++ Front/readly-goods/src/components/pages/Books/Books.jsx
@@ -13,7 +13,6 @@ import Accordion from "../../accordion/Accordion";
 const Books = () => {
   const [books, setBooks] = useState();
   const [filteredBooks, setFilteredBooks] = useState();
-  const [searchTitle, setSearchTitle] = useState('');
   const [searchParams] = useSearchParams();
   const [queryFilter, setQueryFilter] = useState({
     genre: "",
@@ -28,94 +27,56 @@ const Books = () => {
     current: 1,
   });
 
-
-  let url = `https://c15-58-readlygoods-three.vercel.app/books/?genre=${queryFilter.genre}&editorial=${queryFilter.editorial}&author=${queryFilter.author}`;
-
-  const genre = searchParams.get("genre");
-
-
+  let url = `https://c15-58-readlygoods-three.vercel.app/books/?genre=${queryFilter.genre}&editorial=${queryFilter.editorial}&author=${queryFilter.author}&search=${queryFilter.search}`;
   useEffect(() => {
     fetch("https://c15-58-readlygoods-three.vercel.app/books")
       .then((res) => res.json())
       .then((data) => setBooks(data.allBooks));
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
+      window.scrollTo({
+        top:0,
+        behavior:'smooth'
+      })
   }, []);
-
+  const genre = searchParams.get("genre");
   useEffect(() => {
     if (genre) {
-
       setQueryFilter({ ...queryFilter, genre: genre });
-      url = `https://c15-58-readlygoods-three.vercel.app/books/?genre=${genre}&editorial=${queryFilter.editorial}&author=${queryFilter.author}`;
-
+      url = `https://c15-58-readlygoods-three.vercel.app/books/?genre=${genre}&editorial=${queryFilter.editorial}&author=${queryFilter.author}&search=${queryFilter.search}`;
     }
   }, []);
-
   useEffect(() => {
-    fetch(
-      `https://c15-58-readlygoods-three.vercel.app/books/?genre=${queryFilter.genre}&editorial=${queryFilter.editorial}&author=${queryFilter.author}&search=${queryFilter.search}`
-    )
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setFilteredBooks(data.filteredBooks));
-  }, [queryFilter]);
-
-
+  }, [url]);
 
   const handleFilterClick = (e) => {
     const { name, value } = e.target;
-    if (queryFilter[name]) {
-      const found = queryFilter[name].find((e) => e == value);
-      if (found) {
-        handleFilterClickClose(name, value);
-      } else {
-        setQueryFilter({
-          ...queryFilter,
-          [name]: [...queryFilter[name], value],
-          search: "",
-        });
-      }
+    if (queryFilter[name] == value) {
+      setQueryFilter({ ...queryFilter, [name]: "" });
     } else {
       setQueryFilter({
         ...queryFilter,
-        [name]: [...queryFilter[name], value],
-        search: "",
+        [name]: value
       });
     }
   };
 
   const handleFilterClickClose = (name, value) => {
-    if (queryFilter[name]) {
-      const found = queryFilter[name].filter((e) => {
-        if (e !== value) {
-          return e;
-        }
-      });
-      if (found) {
-        setQueryFilter({ ...queryFilter, [name]: found });
-      }
-    }
+    queryFilter[name] !== value &&
+      setQueryFilter({ ...queryFilter, [name]: value });
   };
 
   const handlerOnChangeSearchBar = (e) => {
-
-    // Window.scrollTo(0,0)
     const { value } = e.target;
     setQueryFilter({ ...queryFilter, search: value });
-    const filtered = books.filter(book => book.title.toLowerCase().includes(value.toLowerCase()));
-    console.log(filtered);
-    setFilteredBooks(filtered);
-
   };
 
   const changePage = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    const {dataset} = e.currentTarget
-    console.log(dataset.name)
 
-    if ((name == "previous" || dataset.name == "previous")&& currentPage.current !== 1) {
+    if (name == "previous" && currentPage.current !== 1) {
       setCurrentPage({
         ...currentPage,
         min: currentPage.min - 12,
@@ -123,7 +84,7 @@ const Books = () => {
         current: currentPage.current - 1,
       });
     } else if (
-      (name == "next" || dataset.name == "next") &&
+      name == "next" &&
       currentPage.current !== Math.ceil(filteredBooks.length / 12)
     ) {
       setCurrentPage({
@@ -140,22 +101,15 @@ const Books = () => {
         max: 12 * value,
       });
     }
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-
   };
 
-  console.log()
   const getAllGenre = () => {
     const genres = books?.map((book) => book.genre);
     const allGenres = [...new Set(genres)];
     return allGenres.map((genre) =>
-      queryFilter["genre"]?.indexOf(genre) >= 0 ? (
+      genre === queryFilter.genre ? (
         <button
-          className="bg-[#822626] text-white rounded p-1 "
+          className="bg-[#822626] text-white rounded p-1 text-left"
           key={genre}
           onClick={handleFilterClick}
           name={"genre"}
@@ -165,7 +119,7 @@ const Books = () => {
         </button>
       ) : (
         <button
-          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1"
+          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1 text-left"
           key={genre}
           onClick={handleFilterClick}
           name={"genre"}
@@ -181,9 +135,9 @@ const Books = () => {
     const editorials = books?.map((book) => book.editorial);
     const allEditorials = [...new Set(editorials)];
     return allEditorials.map((editorial) =>
-      queryFilter["editorial"]?.indexOf(editorial) >= 0 ? (
+      editorial === queryFilter.editorial ? (
         <button
-          className="bg-[#822626] text-white rounded p-1"
+          className="bg-[#822626] text-white rounded p-1 text-left"
           key={editorial}
           onClick={handleFilterClick}
           name={"editorial"}
@@ -193,7 +147,7 @@ const Books = () => {
         </button>
       ) : (
         <button
-          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1"
+          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1 text-left"
           key={editorial}
           onClick={handleFilterClick}
           name={"editorial"}
@@ -209,9 +163,9 @@ const Books = () => {
     const Authors = books?.map((book) => book.author);
     const AllAuthor = [...new Set(Authors)];
     return AllAuthor.map((author) =>
-      queryFilter["author"]?.indexOf(author) >= 0 ? (
+      author === queryFilter.author ? (
         <button
-          className="bg-[#822626] text-white rounded p-1"
+          className="bg-[#822626] text-white rounded p-1 text-left"
           key={author}
           onClick={handleFilterClick}
           name={"author"}
@@ -221,7 +175,7 @@ const Books = () => {
         </button>
       ) : (
         <button
-          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1"
+          className="text-gray-500 hover:shadow-gray-300 hover:bg-[#e9cccc] hover:shadow-md rounded p-1 text-left"
           key={author}
           onClick={handleFilterClick}
           name={"author"}
@@ -234,7 +188,7 @@ const Books = () => {
   };
 
   return (
-    <main className="w-full py-12 min-h-screen">
+    <main className="w-full min-h-screen py-12">
       <div className="w-[95%] sm:w-[85%] md:w-[75%] lg:w-[65%] m-auto  flex flex-col gap-6">
         <div className="flex flex-row items-center justify-between ">
           <h1 className="text-sm md:text-xl lg:text-2xl font-semibold uppercase text-[#822626] w-2/6">
@@ -247,7 +201,7 @@ const Books = () => {
               onChange={handlerOnChangeSearchBar}
               type="text"
               placeholder="Busqueda..."
-              className="w-full text-gray-600 border-gray-400 border-solid rounded h-7 lg:h-9 border-1 text-xs sm:text-sm md:text-base"
+              className="w-full text-xs text-gray-600 border-gray-400 border-solid rounded h-7 lg:h-9 border-1 sm:text-sm md:text-base"
             />
           </div>
 
@@ -260,71 +214,48 @@ const Books = () => {
         <div className="w-full flex-1 items-start md:grid md:grid-cols-[240px_minmax(0,1fr)] md:gap-1">
           <aside className="w-full md:sticky md:block">
             <div className="flex flex-wrap gap-3 pb-5">
-              {queryFilter.genre
-                ? queryFilter["genre"].map((x) => {
-                    return (
-                      <button
-                        key={x}
-                        className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded flex gap-2 items-center"
-                        name="genre"
-                      >
-                        {x}{" "}
-                        <FontAwesomeIcon
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleFilterClickClose("genre", x);
-                          }}
-                          icon={faCircleXmark}
-                        />
-                      </button>
-                    );
-                  })
-                : null}
-              {queryFilter.editorial
-                ? queryFilter["editorial"].map((x) => {
-                    return (
-                      <button
-                        key={x}
-                        className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded items-center"
-                        name="editorial"
-                      >
-                        {x}{" "}
-                        <FontAwesomeIcon
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleFilterClickClose("editorial", x);
-                          }}
-                          icon={faCircleXmark}
-                        />
-                      </button>
-                    );
-                  })
-                : null}
-              {queryFilter.author
-                ? queryFilter.author.map((x) => {
-                    return (
-                      <button
-                        key={x}
-                        className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded items-center"
-                        name="author"
-                      >
-                        {x}{" "}
-                        <FontAwesomeIcon
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleFilterClickClose("author", x);
-                          }}
-                          icon={faCircleXmark}
-                        />
-                      </button>
-                    );
-                  })
-                : null}
+              {queryFilter.genre ? (
+                <button
+                  className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded flex gap-2 items-center"
+                  name="genre"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleFilterClickClose("genre", "");
+                  }}
+                >
+                  {queryFilter.genre} <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+              ) : null}
+              {queryFilter.editorial ? (
+                <button
+                  className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded items-center"
+                  name="editorial"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleFilterClickClose("editorial", "");
+                  }}
+                >
+                  {queryFilter.editorial}{" "}
+                  <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+              ) : null}
+              {queryFilter.author ? (
+                <button
+                  className="bg-[#822626] px-2 py-1 text-white hover:bg-[#525252] hover:shadow-md hover:scale-105 transition-all text-sm shadow-slate-300 rounded items-center"
+                  name="author"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleFilterClickClose("author", "");
+                  }}
+                >
+                  {queryFilter.author} <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+              ) : null}
             </div>
             <Accordion
               title={"Género"}
               classTitle={
-                "text-lg font-semibold text-[#822626] hover:bg-gray-200 w-44 px-2 text-left"
+                "text-lg font-semibold text-[#822626] hover:scale-105 w-44 px-2 text-left "
               }
               content={books && getAllGenre()}
               classContent={
@@ -335,7 +266,7 @@ const Books = () => {
             <Accordion
               title={"Editorial"}
               classTitle={
-                "text-lg font-semibold text-[#822626] hover:bg-gray-200 w-44 px-2 text-left"
+                "text-lg font-semibold text-[#822626] hover:scale-105 w-44 px-2 text-left"
               }
               content={books && getAllEditorial()}
               classContent={
@@ -346,7 +277,7 @@ const Books = () => {
             <Accordion
               title={"Autor"}
               classTitle={
-                "text-lg font-semibold text-[#822626] hover:bg-gray-200 w-44 px-2 text-left"
+                "text-lg font-semibold text-[#822626] hover:scale-105 w-44 px-2 text-left"
               }
               content={books && getAllAuthor()}
               classContent={
@@ -356,10 +287,11 @@ const Books = () => {
             />
           </aside>
           <div
-            className={`${filteredBooks?.length > 0
-              ? "grid max-w-5xl grid-cols-1 sm:grid-cols-2 gap-4 mt-0 xl:grid-cols-3"
-              : "flex justify-start items-start flex-col w-full h-full"
-              }`}
+            className={`${
+              filteredBooks?.length > 0
+                ? "grid max-w-5xl grid-cols-1 sm:grid-cols-2 gap-4 mt-0 xl:grid-cols-3"
+                : "flex justify-start items-start flex-col w-full h-full"
+            }`}
           >
             {books?.length > 0 ? (
               filteredBooks?.length > 0 ? (
@@ -402,7 +334,7 @@ const Books = () => {
                   name="previous"
                   className="active:bg-[#822626] text-white h-10 w-10 rounded-s-md hover:scale-150 transition-all"
                 >
-                  <FontAwesomeIcon onClick={changePage} data-name="previous" icon={faArrowLeft} />
+                  <FontAwesomeIcon icon={faArrowLeft} />
                 </button>
                 {filteredBooks?.map((x, index) => {
                   if (index % 12 == 0) {
@@ -421,7 +353,7 @@ const Books = () => {
                     } else
                       return (
                         <button
-                          className="text-white h-10 w-10 hover:scale-150 transition-all"
+                          className="w-10 h-10 text-white transition-all hover:scale-150"
                           onClick={changePage}
                           key={index}
                           value={index / 12 + 1}
@@ -437,7 +369,7 @@ const Books = () => {
                   name="next"
                   className=" hover:border-[#822626] active:bg-[#822626] text-white h-10 w-10 rounded-e-md hover:scale-150 transition-all"
                 >
-                  <FontAwesomeIcon onClick={changePage} data-name="next" icon={faArrowRight} />
+                  <FontAwesomeIcon icon={faArrowRight} />
                 </button>
               </div>
             </div>
